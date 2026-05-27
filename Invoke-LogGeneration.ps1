81e188ab5bc50151044f1f90772f6bc86547850e
Jordan Butler <jordan.butler25@outlook.com>
2026-05-26 21:35:22 -0500

    Update: Refactored existing core components and modified documentation assets

--- Invoke-LogGeneration.ps1
+++ Invoke-LogGeneration.ps1
@@ -1,4 +1,5 @@
-﻿for ($i = 1; $i -le 20; $i++) {
-    net use \\192.168.0.63\C$ /user:kiamsolutions.local\adm-jbutler "BadPassWord_$i"
-    Write-Host "Dropped attack iteration $i/20 on Domain Controller" -ForegroundColor Red
-}
+﻿# Install the Microsoft Graph Identity Module
+Install-Module Microsoft.Graph.Identity.DirectoryManagement -Scope CurrentUser -Force
+
+# Install the Microsoft Graph Users Module
+Install-Module Microsoft.Graph.Users -Scope CurrentUser -Force
